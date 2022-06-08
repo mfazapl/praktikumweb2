22218c48682713eed36c6c2d3c4bbeed6fa67492
mfazapl <101613106+mfazapl@users.noreply.github.com>
2022-06-09 04:13:57 +0700

    Update Controller.php

--- Modul 5/Controller.php
+++ Modul 5/Controller.php
@@ -45,4 +45,101 @@
         header('Location: FormMember.php');
         exit();
     }
+
+ function GetAllDataBuku($pdo) {
+        $con ="SELECT * FROM buku";
+        $statement = $pdo->prepare($con);
+        $statement->execute();
+        $bukus = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $pdo = null;
+        return $bukus;
+    }
+
+    function GetDataBukuById($pdo, $id) {
+        $sql = "SELECT * FROM buku WHERE id_buku =$id";
+        $statement = $pdo ->prepare($sql);
+        $statement -> execute();
+        $bukus = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $pdo = null;
+        return $bukus;
+
+    }
+
+    function UpdateBuku($con, $judul_buku, $penulis, $penerbit, $tahun_terbit, $id) {
+        $sql = "UPDATE buku SET judul_buku=?, penulis=?,penerbit=?, tahun_terbit=?, WHERE id=?";
+        $stmt= $con->prepare($sql);
+        $stmt->execute([ $judul_buku, $penulis, $penerbit, $tahun_terbit, $id]);
+        header('Location: FormBuku.php');
+        exit();
+    }
+    function AddBuku($pdo, $judul_buku, $penulis, $penerbit, $tahun_terbit){
+        $sql = "INSERT INTO buku ( judu_buku, penulis, penerbit, tahun_terbit) VALUE (?,?,?,?) ";
+        $stmt = $pdo ->prepare($sql);
+        $stmt ->execute([$judul_buku, $penulis, $penerbit, $tahun_terbit]);
+        $pdo = null;
+        header("FormBuku.php");
+        exit();
+    }
+
+    function DeleteBuku($con, $id) {
+        $sql = "DELETE FROM buku WHERE id_buku=?";
+        $stmt= $con->prepare($sql);
+        $stmt->execute([$id]);
+        header('Location: FormBuku.php');
+        exit();
+    }
+
+    function GetAllDataPinjam($pdo) {
+        $con ="SELECT * FROM pinjaman";
+        $statement = $pdo->prepare($con);
+        $statement->execute();
+        $pinjams = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $pdo = null;
+        return $pinjams;
+    }
+
+    function GetDataPinjamById($pdo, $id) {
+        $sql = "SELECT * FROM pinjaman WHERE id_pinjaman =$id";
+        $statement = $pdo ->prepare($sql);
+        $statement -> execute();
+        $bukus = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $pdo = null;
+        return $bukus;
+
+    }
+
+    function GetDataPinjam($pdo, $id) {
+        $sql = "SELECT * FROM pinjaman WHERE id_pinjaman =$id";
+        $statement = $pdo ->prepare($sql);
+        $statement -> execute();
+        $pinjams= $statement->fetchAll(PDO::FETCH_ASSOC);
+        $pdo = null;
+        return $pinjams;
+
+    }
+    function UpdatePinjam($con, $tgl_pinjam, $tgl_kembali, $id) {
+        $sql = "UPDATE pinjaman SET tgl_pinjam=?, tgl_kembali=?, WHERE id=?";
+        $stmt= $con->prepare($sql);
+        $stmt->execute([ $tgl_pinjam, $tgl_kembali, $id]);
+        header('Location: FormPinjaman.php');
+        exit();
+    }
+
+    function AddPinjam($pdo, $tgl_pinjam, $tgl_kembali){
+        $sql = "INSERT INTO pinjaman ( tgl_pinjam, tgl_kembali) VALUE (?,?) ";
+        $stmt = $pdo ->prepare($sql);
+        $stmt ->execute([$tgl_pinjam, $tgl_kembali]);
+        $pdo = null;
+        header("FormPinjaman.php");
+        exit();
+    }
+
+    function DeletePinjam($con, $id) {
+        $sql = "DELETE FROM pinjaman WHERE id_pinjam=?";
+        $stmt= $con->prepare($sql);
+        $stmt->execute([$id]);
+        header('Location: FormPinjaman.php');
+        exit();
+    }
+
 ?>
