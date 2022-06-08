663e5f94ea1a264600115995b045201b75b1a0fe
mfazapl <101613106+mfazapl@users.noreply.github.com>
2022-06-09 05:08:55 +0800

    Add files via upload

---
+++ Modul 5/Index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+  <style>
+      a{
+          font-size: 24px;
+          font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+      }
+  </style>
+</head>
+<body>
+
+<nav>
+    <button>
+        <a href="FormBuku.php">Buku</a>
+    </button>
+    <button>
+        <a href="FormMember.php">Member</a>
+    </button>
+    <button>
+        <a href="FormPeminjaman.php">Peminjaman</a>
+    </button>
+</nav>
+
+</body>
+
+</html>
